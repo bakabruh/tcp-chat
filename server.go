e0d3f8470b34315466dc369992cfa96e60618c6e
Tom.L <tom.louveau@edu.devinci.fr>
2025-03-06 21:44:11 +0100

    ADD client connection

--- server.go
+++ server.go
@@ -1,5 +1,10 @@
 package tcpchat
 
+import (
+	"log"
+	"net"
+)
+
 type server struct {
 	rooms    map[string]*room
 	commands chan command
@@ -10,4 +15,16 @@ func newServer() *server {
 		rooms:    make(map[string]*room),
 		commands: make(chan command),
 	}
+}
+
+func (s *server) newClient(conn net.Conn) {
+	log.Printf("new client has connected: %s", conn.RemoteAddr().String())
+
+	c := &client{
+		conn:     conn,
+		nick:     "anonymous",
+		commands: s.commands,
+	}
+
+	c.readInput()
 }
