2cc63dd1d45f31d90a8ea449e0df8a1218fdafb2
Tom.L <tom.louveau@edu.devinci.fr>
2025-03-06 17:22:43 +0100

    ADD server accepting connections

---
+++ main.go
@@ -0,0 +1,28 @@
+package tcpchat
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	s := newServer()
+
+	listener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
+	}
+
+	defer listener.Close()
+	log.Printf("started server on port 8888")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept connection: %s", err.Error())
+			continue
+		}
+
+		go s.newClient(conn)
+	}
+}
